5d661b849e0644fd9287acb39704d1ea6c87cb42
alek <sa48938@zut.edu.pl>
2022-11-23 20:05:25 +0100

    logowanie 0.1v

--- Easy_Travel/app/src/main/java/com/example/easy_travel/MainActivity.kt
+++ Easy_Travel/app/src/main/java/com/example/easy_travel/MainActivity.kt
@@ -3,17 +3,30 @@ package com.example.easy_travel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.Przycisk)
-        button.setOnClickListener{
-            var message = Toast.makeText(applicationContext,"witam",Toast.LENGTH_LONG )
-            message.show()
+        var username = findViewById<TextView>(R.id.username)
+        var password = findViewById<TextView>(R.id.password)
+        var btn = findViewById<MaterialButton>(R.id.login_button)
+        //admin and admin
+
+        btn.setOnClickListener {
+            if(username.text.toString().equals("admin") && password.text.toString().equals("admin")){
+                var mes1 =Toast.makeText(applicationContext, "LOGIN SUCCESFUL", Toast.LENGTH_LONG)
+                mes1.show()
+            }
+            else{
+                var mes2 =Toast.makeText(applicationContext, "LOGIN FAILED", Toast.LENGTH_LONG)
+                mes2.show()
+            }
         }
+
     }
 }
