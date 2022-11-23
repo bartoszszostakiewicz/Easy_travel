b09fd0f2ce8d8de418dbfc27c0c99766b8ab52c6
alek <sa48938@zut.edu.pl>
2022-11-23 14:19:07 +0100

    pierwszy przycisk

--- Easy_Travel/app/src/main/java/com/example/easy_travel/MainActivity.kt
+++ Easy_Travel/app/src/main/java/com/example/easy_travel/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.easy_travel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.Przycisk)
+        button.setOnClickListener{
+            var message = Toast.makeText(applicationContext,"witam",Toast.LENGTH_LONG )
+            message.show()
+        }
     }
 }
