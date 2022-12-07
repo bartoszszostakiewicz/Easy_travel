d7a5080a99fa530c11aeef690acfbd8bfe1bbe06
Bartek <sb49417@zut.edu.pl>
2022-12-07 14:52:38 +0100

    Resend work

--- Easy_Travel/app/src/main/java/com/example/easy_travel/MainActivity.kt
+++ Easy_Travel/app/src/main/java/com/example/easy_travel/MainActivity.kt
@@ -1,36 +1,104 @@
 package com.example.easy_travel
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
+import java.sql.Connection
+import java.sql.DriverManager
+
+import java.sql.SQLException
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         var username = findViewById<TextView>(R.id.username)
         var password = findViewById<TextView>(R.id.password)
         var btn = findViewById<MaterialButton>(R.id.login_button)
         //admin and admin
 
         btn.setOnClickListener {
-            if(username.text.toString().equals("admin") && password.text.toString().equals("admin")){
-                var mes1 =Toast.makeText(applicationContext, "LOGIN SUCCESFUL", Toast.LENGTH_LONG)
+        Log.d("TAD", "Button clicked")
+
+
+            Log.d("TAD", "Connection established");
+            if (username.text.toString() == "admin" || password.text.toString() == "") {
+                var mes1 = Toast.makeText(applicationContext, "LOGIN SUCCESFUL", Toast.LENGTH_LONG)
                 mes1.show()
 
-                var nowaAktywnosc: Intent = Intent(applicationContext, ListaWycieczek::class.java)
+                var nowaAktywnosc: Intent = Intent(applicationContext, Menu::class.java)
                 startActivity(nowaAktywnosc)
-            }
-            else{
-                var mes2 =Toast.makeText(applicationContext, "LOGIN FAILED", Toast.LENGTH_LONG)
+            } else {
+                var mes2 = Toast.makeText(applicationContext, "LOGIN FAILED", Toast.LENGTH_LONG)
                 mes2.show()
             }
         }
 
     }
 }
+
+fun connectionClass(): Connection? {
+    Log.d("TAD", "ConnectionClass")
+    var policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+    StrictMode.setThreadPolicy(policy)
+    var connection: Connection? = null
+    var ConnURL: String? = null
+    try {
+        ConnURL = "jdbc:sqlserver://mysqlserverpow.database.windows.net:1433;database=Easy_travel_db;user=sb49417@mysqlserverpow;password=Easy_travel;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;"
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+
+        connection =
+            DriverManager.getConnection(ConnURL)
+        Log.d("TAD", "Połączono")
+    } catch (se: SQLException) {
+        se.message?.let { Log.e("ERROR", it) }
+    } catch (e: ClassNotFoundException) {
+        e.message?.let { Log.e("ERROR", it) }
+    } catch (e: Exception) {
+        e.message?.let { Log.e("ERROR", it) }
+
+
+    }
+        return connection
+}
+
+fun checkLogin(username: String, password: String): Boolean {
+    /*
+    Log.d("TAD", "CheckLogin")
+    var connection: Connection? = null
+    var result: Boolean = false
+    try {
+        connection = connectionClass()
+        if (connection == null) {
+            Log.d("TAD", "Connection is null")
+            result = false
+        } else {
+            /**
+            var query: String =
+            "select * from [dbo].[logowanie] where imie = '$username' and nazwisko = '$password'"
+            var stmt = connection!!.createStatement()
+            var rs = stmt.executeQuery(query)
+            if (rs.next()) {
+            result = true
+            } else {
+            result = false
+            }
+             */
+            result = true
+        }
+    } catch (ex: Exception) {
+        with(ex) { printStackTrace() }
+    }
+
+     */
+    return false
+}
+
+
---
+++ Easy_Travel/app/src/main/java/com/example/easy_travel/Menu.kt
@@ -0,0 +1,13 @@
+package com.example.easy_travel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_map)
+    }
+
+
+}
