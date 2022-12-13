5d47c93f817ee82ec344287d885fd5e5fe7acba1
alek <sa48938@zut.edu.pl>
2022-12-14 00:52:09 +0100

    organizacja 0.1

--- Easy_Travel/app/src/main/java/com/example/easy_travel/MainActivity.kt
+++ Easy_Travel/app/src/main/java/com/example/easy_travel/MainActivity.kt
@@ -20,20 +20,21 @@ class MainActivity : AppCompatActivity() {
         var username = findViewById<TextView>(R.id.username)
         var password = findViewById<TextView>(R.id.password)
         var btn = findViewById<MaterialButton>(R.id.login_button)
-        //admin and admin
 
         btn.setOnClickListener {
         Log.d("TAD", "Button clicked")
 
 
             Log.d("TAD", "Connection established");
-            if (username.text.toString() == "admin" || password.text.toString() == "") {
+            if (username.text.toString() == "admin" && password.text.toString() == "") {
                 var mes1 = Toast.makeText(applicationContext, "LOGIN SUCCESFUL", Toast.LENGTH_LONG)
                 mes1.show()
 
                 var nowaAktywnosc: Intent = Intent(applicationContext, MenuActivity::class.java)
                 startActivity(nowaAktywnosc)
-            } else {
+            }
+
+            else {
                 var mes2 = Toast.makeText(applicationContext, "LOGIN FAILED", Toast.LENGTH_LONG)
                 mes2.show()
             }
