f988dbd9ba5ba356a91bf6d2f8d017e6a27d28ca
bartek <bartek64377@gmail.com>
2022-12-14 14:09:11 +0100

    add user class, test activity to db

--- Easy_Travel/app/src/main/java/com/example/easy_travel/MenuActivity.kt
+++ Easy_Travel/app/src/main/java/com/example/easy_travel/MenuActivity.kt
@@ -1,6 +1,8 @@
 package com.example.easy_travel
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -8,5 +10,14 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_map)
+
+        var btn = findViewById<Button>(R.id.button5)
+
+        btn.setOnClickListener{
+            var database_test: Intent = Intent(applicationContext,Database_test::class.java)
+            startActivity(database_test)
+
+        }
+
     }
 }
