df4372c1c60711ab4af9abfdbcd90fb6b9279a8a
alek <sa48938@zut.edu.pl>
2023-01-11 14:06:15 +0100

    dziala Organizacja

--- Easy_Travel/app/src/main/java/com/project/easy_travel/Organizacja.kt
+++ Easy_Travel/app/src/main/java/com/project/easy_travel/Organizacja.kt
@@ -1,38 +1,34 @@
 package com.project.easy_travel
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.button.MaterialButton
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.project.easy_travel.Model.Wycieczka
+import com.project.easy_travel.ViewModel.writeNewTrip
 
-//import kotlinx.android.synthetic.main.activity_organizacja.*
-
-//import kotlinx.android.synthetic.main.activity_organizacja.*
+import kotlinx.android.synthetic.main.activity_organizacja.*
 
 class Organizacja : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_organizacja)
 
+
         var name = findViewById<TextView>(R.id.nazwa_wycieczki)
         var przew = findViewById<TextView>(R.id.przewodnik)
+        var ops = findViewById<TextView>(R.id.opis)
+
         var btn = findViewById<MaterialButton>(R.id.stworz_button)
 
-        val Lista_wycieczek: MutableList<Wycieczka> = mutableListOf()
+        //val Lista_wycieczek: MutableList<Wycieczka> = mutableListOf()
 
         btn.setOnClickListener {
-
-            var pierwsza = Wycieczka(name, przew)
-            var n = pierwsza.name
-            var p = pierwsza.przewodnik
-
-            Lista_wycieczek.add(pierwsza)
-            var key =pierwsza.get_key()
-
-            //Nowa_wycieczka.text= key.toString()
+            writeNewTrip(name.text.toString(),przew.text.toString(),ops.text.toString(),"0")
+            //Lista_wycieczek.add(pierwsza)
         }
 
     }
